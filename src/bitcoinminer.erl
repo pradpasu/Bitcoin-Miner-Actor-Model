798597129a0a0a47e8baf48af94f8f75d718f2af
Pradyumna Pasumarty <pradyumnapasumarty1997@gmail.com>
2022-09-19 16:16:03 -0400

    ref(bitcoinminer.erl): implemented boss actor and communication

--- src/bitcoinminer.erl
+++ src/bitcoinminer.erl
@@ -2,38 +2,39 @@
 
 -import(binary, [decode_unsigned/1]).
 -import(crypto, [hash/1]).
--export([getRandomStringFromCrypto/0, performMiningRecursively/2, getCoins/1, miner/0]).
+-export([getRandomStringFromCrypto/0, performMiningRecursively/3, getCoins/1, miner/0, collectCoins/0]).
 
-performMiningRecursively(NumberOfLeadingZeroes, Counter) ->
+performMiningRecursively(NumberOfLeadingZeroes, Counter, BossActor) ->
 	CommonPrefix = "ppasumarty",
-	PrefixAndKeySeperator = ";",
+	PrefixAndKeySeparator = ";",
 	RandomString = bitcoinminer:getRandomStringFromCrypto(),
-	StringToBeHashed = string:concat(CommonPrefix, string:concat(PrefixAndKeySeperator, RandomString)),
+	StringToBeHashed = string:concat(CommonPrefix, string:concat(PrefixAndKeySeparator, RandomString)),
 	HashAsInteger = binary:decode_unsigned(crypto:hash(sha256,StringToBeHashed)),
 	HashAsString = io_lib:format("~64.16.0b", [HashAsInteger]),
 	LeadingZeroesString = io_lib:format("~*..0b", [NumberOfLeadingZeroes, 0]),
 	case (string:str(HashAsString, LeadingZeroesString) == 1) of
 	    true -> 
 			NewCounter = Counter + 1,
-			io:fwrite("(~p): ", [NewCounter]),
-			io:fwrite("Hashed Value of ~p is ~p~n", [StringToBeHashed, HashAsString]),
+			BossActor ! {StringToBeHashed, HashAsString},
 			if 
 				NewCounter < 5 ->
-					bitcoinminer:performMiningRecursively(NumberOfLeadingZeroes, NewCounter);
+					bitcoinminer:performMiningRecursively(NumberOfLeadingZeroes, NewCounter, BossActor);
 				true ->
-					io:fwrite("")
+					ok
 			end;
 	    false ->
-			bitcoinminer:performMiningRecursively(NumberOfLeadingZeroes, Counter)
+			bitcoinminer:performMiningRecursively(NumberOfLeadingZeroes, Counter, BossActor)
   	end.
 
 getRandomStringFromCrypto() -> base64:encode_to_string(crypto:strong_rand_bytes(3)).
 
-getCoins(NumberOfLeadingZeroes) -> 
+getCoins(NumberOfLeadingZeroes) ->
+	BossActor = spawn(fun bitcoinminer:collectCoins/0),
+	BossActor ! {'initialize coin collection'},
 	NumberOfActors = 4,
 	lists:foldl(
 		fun(_, _) -> 
-			spawn(fun bitcoinminer:miner/0) ! {self(), NumberOfLeadingZeroes} 
+			spawn(fun bitcoinminer:miner/0) ! {self(), NumberOfLeadingZeroes, BossActor}
 		end, 
 		[], 
 		lists:seq(1, NumberOfActors)
@@ -41,7 +42,19 @@ getCoins(NumberOfLeadingZeroes) ->
 
 miner() ->
 	receive
-		{From, NumberOfLeadingZeroes} -> 
-			bitcoinminer:performMiningRecursively(NumberOfLeadingZeroes, 0),
-			io:fwrite("Mining finished by Actor ~p", [From])
+		{_, NumberOfLeadingZeroes, BossActor} ->
+			bitcoinminer:performMiningRecursively(NumberOfLeadingZeroes, 0, BossActor)
 	end.
+
+collectCoins() ->
+	receive
+		{'initialize coin collection'} ->
+			io:fwrite("Coin collecting initialized in Boss Actor ~n"),
+			collectCoins();
+		{StringToBeHashed, HashedString} ->
+			io:fwrite("~p	~p~n", [StringToBeHashed, HashedString]),
+			collectCoins()
+	end.
+
+
+%% cd("E:/DOSP Projects/Bitcoin-Miner-Actor-Model/src").
