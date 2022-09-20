8d0c61288a2477c68bcfefc0ad5425bbd1e18a73
Pradyumna Pasumarty <pradyumnapasumarty1997@gmail.com>
2022-09-20 00:57:41 -0400

    ref(bitcoinminer.erl): standardized code

--- src/bitcoinminer.erl
+++ src/bitcoinminer.erl
@@ -3,12 +3,12 @@
 -import(binary, [decode_unsigned/1]).
 -import(crypto, [hash/1]).
 -export([getRandomStringFromCrypto/0, performMiningRecursively/3, getCoins/1, miner/0, collectCoins/0]).
+-define(CommonPrefix, "ppasumarty").
+-define(PrefixAndKeySeparator, ";").
 
 performMiningRecursively(NumberOfLeadingZeroes, Counter, BossActor) ->
-	CommonPrefix = "ppasumarty",
-	PrefixAndKeySeparator = ";",
 	RandomString = bitcoinminer:getRandomStringFromCrypto(),
-	StringToBeHashed = string:concat(CommonPrefix, string:concat(PrefixAndKeySeparator, RandomString)),
+	StringToBeHashed = string:concat(?CommonPrefix, string:concat(?PrefixAndKeySeparator, RandomString)),
 	HashAsInteger = binary:decode_unsigned(crypto:hash(sha256,StringToBeHashed)),
 	HashAsString = io_lib:format("~64.16.0b", [HashAsInteger]),
 	LeadingZeroesString = io_lib:format("~*..0b", [NumberOfLeadingZeroes, 0]),
@@ -17,7 +17,7 @@ performMiningRecursively(NumberOfLeadingZeroes, Counter, BossActor) ->
 			NewCounter = Counter + 1,
 			BossActor ! {StringToBeHashed, HashAsString},
 			if 
-				NewCounter < 5 ->
+				NewCounter < 50 ->
 					bitcoinminer:performMiningRecursively(NumberOfLeadingZeroes, NewCounter, BossActor);
 				true ->
 					ok
@@ -26,11 +26,10 @@ performMiningRecursively(NumberOfLeadingZeroes, Counter, BossActor) ->
 			bitcoinminer:performMiningRecursively(NumberOfLeadingZeroes, Counter, BossActor)
   	end.
 
-getRandomStringFromCrypto() -> base64:encode_to_string(crypto:strong_rand_bytes(3)).
+getRandomStringFromCrypto() -> base64:encode_to_string(crypto:strong_rand_bytes(6)).
 
 getCoins(NumberOfLeadingZeroes) ->
 	BossActor = spawn(fun bitcoinminer:collectCoins/0),
-	BossActor ! {'initialize coin collection'},
 	NumberOfActors = 4,
 	lists:foldl(
 		fun(_, _) -> 
@@ -48,13 +47,7 @@ miner() ->
 
 collectCoins() ->
 	receive
-		{'initialize coin collection'} ->
-			io:fwrite("Coin collecting initialized in Boss Actor ~n"),
-			collectCoins();
 		{StringToBeHashed, HashedString} ->
 			io:fwrite("~p	~p~n", [StringToBeHashed, HashedString]),
 			collectCoins()
 	end.
-
-
-%% cd("E:/DOSP Projects/Bitcoin-Miner-Actor-Model/src").
